f1af646a793ba5f2dc6dfef7298f04ee984ea683
Pomidorchics <aleksandra180504@gmail.com>
2025-09-04 00:46:08 +1000

    video

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Block1 from './components/Block1';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Block1/>
   </React.StrictMode>
 );
 
